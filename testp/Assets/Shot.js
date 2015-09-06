e14ace4d5380fe55f19fe5dd23398212ac5b84fb
sakiccho <sakiccho01@gmail.com>
2015-09-06 20:35:44 +0900

    bullet direction fix, and more.
    1.bullet color change to gray.
    2.bullet speed 1000 to 10000.

--- testp/Assets/Shot.js
+++ testp/Assets/Shot.js
@@ -1,6 +1,6 @@
 ﻿public var bullet : GameObject; //GameObject型変数bulletを宣言
 public var spawn : Transform;   //Transform型変数spawnを宣言
-public var speed : float = 1000;//float型変数speedを宣言。同時に1000を代入
+public var speed : float = 10000;//float型変数speedを宣言。同時に1000を代入
  
 public var rifle : Transform;   //修正箇所。銃オブジェクトのTransformを参照にする
 public var interval : float = 0.07;  //何秒おきに
@@ -32,7 +32,9 @@ function Shot (){
 	sound.PlayOneShot(shotSound);
     var obj : GameObject = GameObject.Instantiate(bullet);  //GameObject型変数objを宣言
     obj.transform.position = spawn.position;                //objに代入されたオブジェクトの位置をspawnオブジェクトに合わせる
-    obj.GetComponent.<Rigidbody>().AddForce(transform.forward * speed);         //修正点
+    var direction = (spawn.position - transform.position).normalized;
+    direction.y -= 0.4;	//adjust
+    obj.GetComponent.<Rigidbody>().AddForce(direction * speed);         //修正点
     //注意・今回のチュートリアルで使った銃は、向きが反転していたため、前後が逆になっています。そのため”-”を付けて向きを逆転させています        
     //objオブジェクトに付与されたrigidbodyを利用して前方向に1000の力を加える
 }
