baf960a949f7f1bd908e9956539992c0ab41f836
sakiccho <sakiccho01@gmail.com>
2015-09-21 23:34:59 +0900

    add scorpion
    boss position fixed.

--- testp/Assets/01.Script/EnemySpawn.js
+++ testp/Assets/01.Script/EnemySpawn.js
@@ -2,12 +2,11 @@
 public var interval : float = 6;//何秒おきに敵を発生させるか
 public var count : float = 2;   //一度にスポーンさせる敵の数
 public var enemy : GameObject;  //敵オブジェクト
-public var boss : GameObject;  //bossオブジェクト
+public var boss : GameObject;
 static public var enemyCount : int;	//enemy count on field
-private var isQuitting : boolean = false;   //ゲーム終了後のオブジェクト生成回避
-public var maxEnemy : int = 1;
 public var totalEnemy : int = 0;
 public var bossCount : int = 0;
+private var isQuitting : boolean = false;   //ゲーム終了後のオブジェクト生成回避
  
 function Start () {
     Spawn();    //初期スポーン
@@ -15,12 +14,10 @@ function Start () {
 }
  
 function Update () {
-	Debug.Log(totalEnemy);
-	if(bossCount == 0){
-	    if(enemyCount <= 0){
-	        Spawn();
-	        totalEnemy += 1;
-	    }
+    if(enemyCount <= 0 && totalEnemy <= 5){
+        Spawn();
+    } else if(totalEnemy > 5 && enemyCount == 0) {
+    	bossSpawn();
     }
 }
  function OnApplicationQuit () {
@@ -29,29 +26,26 @@ function Update () {
 }
 function Spawn () {
 	if(!isQuitting){
-		if(totalEnemy < maxEnemy) {
-		    for(var i =0; i < count ; i++) {
-		    //ランダム生成する際の範囲が-25~25なのは地面の大きさが50だから
-		        var x : float = Random.Range(-35f, 35f);    //x座標をランダム生成
-		        var z : float = Random.Range(-35f, 35f);    //z座標をランダム生成
-		        var pos : Vector3 = Vector3(x, 5, z );//ランダム生成の位置座標を作成
-		        pos += Vector3(0, 0, 0 );
-		        
-		        GameObject.Instantiate(enemy, pos, Quaternion.identity);
-		        //第一引数は生成するオブジェクト
-		        //第二引数は生成する座標
-		        //第三引数は生成したオブジェクトの向き
-		        enemyCount += 1;
-		    }
-		} else {
-				//spawn boss when enemy count max
-		        x = Random.Range(-35f, 35f);    //x座標をランダム生成
-		        z = Random.Range(-35f, 35f);    //z座標をランダム生成
-		        pos = Vector3(x, 5, z );//ランダム生成の位置座標を作成
-		        pos += Vector3(0, 0, 0 );
-		        
-		        GameObject.Instantiate(boss, pos, Quaternion.identity);
-		        bossCount += 1;
-		        }
+	    for(var i =0; i < count ; i++) {
+	    //ランダム生成する際の範囲が-25~25なのは地面の大きさが50だから
+	        var x : float = Random.Range(-35f, 35f);    //x座標をランダム生成
+	        var z : float = Random.Range(-35f, 35f);    //z座標をランダム生成
+	        var pos : Vector3 = Vector3(x, 5, z );//ランダム生成の位置座標を作成
+	        pos += Vector3(0, 0, 0 );
+	        
+	        GameObject.Instantiate(enemy, pos, Quaternion.identity);
+	        //第一引数は生成するオブジェクト
+	        //第二引数は生成する座標
+	        //第三引数は生成したオブジェクトの向き
+	        enemyCount += 1;
+	        totalEnemy += 1;
+	    }
+	}
+}
+function bossSpawn () {
+	if(!isQuitting && bossCount == 0){
+		var pos : Vector3 = Vector3(0, 14, 0);//ランダム生成の位置座標を作成
+        GameObject.Instantiate(boss, pos, Quaternion.identity);
+        bossCount += 1;
 	}
 }
---
+++ testp/Assets/01.Script/scpSpawn.js
@@ -0,0 +1,39 @@
+﻿public var ground : Transform;  //地面オブジェクト
+public var interval : float = 5;//何秒おきに敵を発生させるか
+public var count : float = 1;   //一度にスポーンさせる敵の数
+public var enemy : GameObject;  //敵オブジェクト
+static public var enemyCount : int;	//enemy count on field
+private var isQuitting : boolean = false;   //ゲーム終了後のオブジェクト生成回避
+public var boss : GameObject;
+public var scpSpawn : Transform;   //Transform型変数spawnを宣言
+private var timer : float;
+ 
+function Start () {
+    Spawn();    //初期スポーン
+    
+}
+ 
+function Update () {
+    timer += Time.deltaTime;
+    if(timer >= interval){
+        Spawn();
+        timer = 0;
+    }
+}
+ function OnApplicationQuit () {
+//ゲームは終了しているか
+    isQuitting = true;
+}
+function Spawn () {
+	if(!isQuitting){
+	    for(var i =0; i < count ; i++) {
+	        var pos : Vector3 = transform.position;
+	        
+	        GameObject.Instantiate(enemy, pos, Quaternion.identity);
+	        //第一引数は生成するオブジェクト
+	        //第二引数は生成する座標
+	        //第三引数は生成したオブジェクトの向き
+	        enemyCount += 1;
+	    }
+	}
+}
